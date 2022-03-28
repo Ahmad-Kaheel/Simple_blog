7ccaed10ec80093672a862ccd704a3ac2dd46f07
Ahmad-Kaheel <ahmadkahil596@gmail.com>
2022-03-28 21:06:00 +0300

    change debug to false

--- blog_lyfe/settings.py
+++ blog_lyfe/settings.py
@@ -32,7 +32,7 @@ if os.path.isfile(dotenv_file):
 SECRET_KEY = 'django-insecure-l-_lzjw2btw*&v^v7^fes6h&!#lbl&=4(-%v_i4jgpe-%neygg'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['aroundtheword.herokuapp.com','127.0.0.1:8000', 'localhost']
 
