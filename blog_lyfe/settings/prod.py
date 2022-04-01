4770ec9c8d6c9bf3fda2748e29845f7e9546b1e4
Ahmad-Kaheel <ahmadkahil596@gmail.com>
2022-04-01 18:48:50 +0300

    Edit settings.py

--- blog_lyfe/settings/prod.py
+++ blog_lyfe/settings/prod.py
@@ -84,3 +84,8 @@ LOGGING = {
 #------------------------Heroku settings--------------------------------------------
 django_on_heroku.settings(locals(), staticfiles=False) # To make heroku not take care of serving staticfiles
 del DATABASES['default']['OPTIONS']['sslmode'] # To remove all mistakes in database
+
+#--------------------test-------------------
+import mimetypes
+
+mimetypes.add_type("text/javascript", ".js", True)
