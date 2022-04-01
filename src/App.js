843ac5c61156d2ad659b0435bd2e1b5936c5c54a
Ahmad-Kaheel <ahmadkahil596@gmail.com>
2022-04-01 23:08:25 +0300

    Switching Browsrouter into hashrouter

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router , Routes, Route } from "react-router-dom";
+import { HashRouter as Router , Routes, Route } from "react-router-dom";
 import Layout from './hocs/Layout';
 import Blog from './components/Blog';
 import BlogDetail from './components/BlogDetail';
