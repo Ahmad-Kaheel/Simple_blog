4b13556606f4ed3395e77d3513090e072f7793d8
Ahmad-Kaheel <ahmadkahil596@gmail.com>
2022-04-18 08:15:22 +0300

    Edit settings.py

--- blog_lyfe/settings/prod.py
+++ blog_lyfe/settings/prod.py
@@ -9,7 +9,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['aroundtheword.herokuapp.com/']
+ALLOWED_HOSTS = ['aroundtheword.herokuapp.com']
 
 
 
